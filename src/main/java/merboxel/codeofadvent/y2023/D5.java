5fb687d164c38f54d8ac6b75a40373dc8e69bd0e
Max <m.e.r.van.boxel@gmail.com>
2023-12-04 22:29:35 +0100

    Prep day5

---
+++ src/main/java/merboxel/codeofadvent/y2023/D5.java
@@ -0,0 +1,53 @@
+package merboxel.codeofadvent.y2023;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static merboxel.codeofadvent.FileReader.readFileAsScanner;
+
+public class D5 {
+
+    public static void main(String[] args) throws IOException {
+        D5P1.run();
+        D5P2.run();
+    }
+}
+
+class D5P1 {
+
+    public static void main(String[] args) throws IOException {
+        run();
+    }
+
+    public static void run() throws IOException {
+        System.out.println("--------------- Part 1 ---------------");
+        Scanner sc = readFileAsScanner(2023, 5);
+
+        while(sc.hasNext()) {
+            String line = sc.nextLine();
+        }
+        System.out.println("--------------------------------------");
+    }
+}
+
+class D5P2 {
+
+    public static void main(String[] args) throws IOException {
+        run();
+    }
+
+    public static void run() throws IOException {
+        System.out.println("--------------- Part 2 ---------------");
+        Scanner sc = readFileAsScanner(2023, 5);
+
+        while(sc.hasNext()) {
+            String line = sc.nextLine();
+        }
+        System.out.println("--------------------------------------");
+    }
+}
