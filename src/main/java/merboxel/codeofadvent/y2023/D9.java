b65ac01ad2bb4277879a763ca1b66c1a069eeafe
Max <m.e.r.van.boxel@gmail.com>
2023-12-09 08:21:32 +0100

    D9 complete

--- src/main/java/merboxel/codeofadvent/y2023/D9.java
+++ src/main/java/merboxel/codeofadvent/y2023/D9.java
@@ -4,6 +4,7 @@ import merboxel.codeofadvent.util.PatternMatching;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.LongStream;
 
 import static merboxel.codeofadvent.FileReader.readFileAsScanner;
 
@@ -31,10 +32,45 @@ class D9P1 {
         Scanner sc = D9.readFile();
 
         while(sc.hasNext()) {
+            long[] sequenceOrr = PatternMatching.getLongsWithNegativesAsArray(sc.nextLine());
+            long[] tmp = Arrays.copyOf(sequenceOrr,sequenceOrr.length);
+            List<Long> lastDigits = new ArrayList<>();
 
+            lastDigits.add(tmp[tmp.length-1]);
+
+            for(int i = 1; i < sequenceOrr.length; i++) {
+                tmp = Calc.calcDiff(tmp);
+                // System.out.println(Arrays.toString(tmp)); //Debug
+                lastDigits.add(tmp[tmp.length-1]);
+                if(Calc.allZero(tmp))
+                    break;
+            }
+
+            long x = 0;
+            for(long l: lastDigits.reversed()) {
+                //System.out.println(x + " " + l); //Debug
+                x += l;
+            }
+            result += x;
         }
         System.out.println(result);
         System.out.println("--------------------------------------");
+    }
+}
+
+class Calc {
+
+    public static boolean allZero(long[] seq){
+        for(long l: seq)
+            if(l != 0L)
+                return false;
+        return true;
+    }
+
+    public static long[] calcDiff(long[] seq) {
+        return LongStream.range(0,seq.length-1)
+                .map(i -> seq[((int)i+1)]-seq[((int)i)])
+                .toArray();
     }
 }
 
@@ -50,7 +86,25 @@ class D9P2 {
         Scanner sc = D9.readFile();
 
         while(sc.hasNext()) {
+            long[] sequenceOrr = PatternMatching.getLongsWithNegativesAsArray(sc.nextLine());
+            long[] tmp = Arrays.copyOf(sequenceOrr,sequenceOrr.length);
+            List<Long> firstDigits = new ArrayList<>();
 
+            firstDigits.add(tmp[0]);
+
+            for(int i = 1; i < sequenceOrr.length; i++) {
+                tmp = Calc.calcDiff(tmp);
+                // System.out.println(Arrays.toString(tmp)); //Debug
+                firstDigits.add(tmp[0]);
+                if(Calc.allZero(tmp))
+                    break;
+            }
+            long x = 0;
+            for(long l: firstDigits.reversed()) {
+                x = l - x;
+                //System.out.println(x + " " + l); //Debug
+            }
+            result += x;
         }
         System.out.println(result);
         System.out.println("--------------------------------------");
