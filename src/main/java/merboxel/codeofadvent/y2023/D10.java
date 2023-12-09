310b4a3f1b86c21439d1da33dbf9b4d28b9354fc
Max <m.e.r.van.boxel@gmail.com>
2023-12-09 08:26:25 +0100

    Prep day 10

---
+++ src/main/java/merboxel/codeofadvent/y2023/D10.java
@@ -0,0 +1,115 @@
+package merboxel.codeofadvent.y2023;
+
+import merboxel.codeofadvent.util.PatternMatching;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.LongStream;
+
+import static merboxel.codeofadvent.FileReader.readFileAsScanner;
+
+public class D10 {
+
+    public static void main(String[] args) throws IOException {
+        D10P1.run();
+        D10P2.run();
+    }
+
+    static Scanner readFile() throws IOException {
+        return readFileAsScanner(2023, 10);
+    }
+}
+
+class D10P1 {
+
+    public static void main(String[] args) throws IOException {
+        run();
+    }
+
+    public static void run() throws IOException {
+        System.out.println("--------------- Part 1 ---------------");
+        long result = 0;
+        Scanner sc = D10.readFile();
+
+        while(sc.hasNext()) {
+            long[] sequenceOrr = PatternMatching.getLongsWithNegativesAsArray(sc.nextLine());
+            long[] tmp = Arrays.copyOf(sequenceOrr,sequenceOrr.length);
+            List<Long> lastDigits = new ArrayList<>();
+
+            lastDigits.add(tmp[tmp.length-1]);
+
+            for(int i = 1; i < sequenceOrr.length; i++) {
+                tmp = Calc.calcDiff(tmp);
+                // System.out.println(Arrays.toString(tmp)); //Debug
+                lastDigits.add(tmp[tmp.length-1]);
+                if(Calc.allZero(tmp))
+                    break;
+            }
+
+            long x = 0;
+            for(long l: lastDigits.reversed()) {
+                //System.out.println(x + " " + l); //Debug
+                x += l;
+            }
+            result += x;
+        }
+        System.out.println(result);
+        System.out.println("--------------------------------------");
+    }
+}
+
+class Calc {
+
+    public static boolean allZero(long[] seq){
+        for(long l: seq)
+            if(l != 0L)
+                return false;
+        return true;
+    }
+
+    public static long[] calcDiff(long[] seq) {
+        return LongStream.range(0,seq.length-1)
+                .map(i -> seq[((int)i+1)]-seq[((int)i)])
+                .toArray();
+    }
+}
+
+class D10P2 {
+
+    public static void main(String[] args) throws IOException {
+        run();
+    }
+
+    public static void run() throws IOException {
+        System.out.println("--------------- Part 2 ---------------");
+        long result = 0L;
+        Scanner sc = D10.readFile();
+
+        while(sc.hasNext()) {
+            long[] sequenceOrr = PatternMatching.getLongsWithNegativesAsArray(sc.nextLine());
+            long[] tmp = Arrays.copyOf(sequenceOrr,sequenceOrr.length);
+            List<Long> firstDigits = new ArrayList<>();
+
+            firstDigits.add(tmp[0]);
+
+            for(int i = 1; i < sequenceOrr.length; i++) {
+                tmp = Calc.calcDiff(tmp);
+                // System.out.println(Arrays.toString(tmp)); //Debug
+                firstDigits.add(tmp[0]);
+                if(Calc.allZero(tmp))
+                    break;
+            }
+            long x = 0;
+            for(long l: firstDigits.reversed()) {
+                x = l - x;
+                //System.out.println(x + " " + l); //Debug
+            }
+            result += x;
+        }
+        System.out.println(result);
+        System.out.println("--------------------------------------");
+    }
+}
