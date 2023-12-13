6895127ed916f45435bf51c093ba2d9a16d6f493
Max <m.e.r.van.boxel@gmail.com>
2023-12-13 09:02:16 +0100

    D14 prep

---
+++ src/main/java/merboxel/codeofadvent/y2023/D14.java
@@ -0,0 +1,58 @@
+package merboxel.codeofadvent.y2023;
+
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static merboxel.codeofadvent.FileReader.readFileAsScanner;
+
+public class D14 {
+
+    public static void main(String[] args) throws IOException {
+        P1.run();
+        P2.run();
+    }
+
+    static Scanner readFile() throws IOException {
+        return readFileAsScanner(2023, 13);
+    }
+
+    static class P1 {
+
+        public static void main(String[] args) throws IOException {
+            run();
+        }
+
+        public static void run() throws IOException {
+            System.out.println("--------------- Part 1 ---------------");
+            long result = 0L;
+            Scanner sc = readFile();
+
+            while (sc.hasNext()) {
+            }
+
+            System.out.println(result);
+            System.out.println("--------------------------------------");
+        }
+    }
+
+    static class P2 {
+
+        public static void main(String[] args) throws IOException {
+            run();
+        }
+
+        public static void run() throws IOException {
+            System.out.println("--------------- Part 2 ---------------");
+            long result = 0L;
+            Scanner sc = readFile();
+
+            while (sc.hasNext()) {
+            }
+
+            System.out.println(result);
+            System.out.println("--------------------------------------");
+        }
+    }
+}
