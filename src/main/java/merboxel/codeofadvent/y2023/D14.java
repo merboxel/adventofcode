280ae471f15d442b96e2561030fcf3a380a2efd6
Max <m.e.r.van.boxel@gmail.com>
2023-12-14 08:24:17 +0100

    D14

--- src/main/java/merboxel/codeofadvent/y2023/D14.java
+++ src/main/java/merboxel/codeofadvent/y2023/D14.java
@@ -3,7 +3,6 @@ package merboxel.codeofadvent.y2023;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static merboxel.codeofadvent.FileReader.readFileAsScanner;
 
@@ -15,7 +14,7 @@ public class D14 {
     }
 
     static Scanner readFile() throws IOException {
-        return readFileAsScanner(2023, 13);
+        return readFileAsScanner(2023, 14);
     }
 
     static class P1 {
@@ -29,7 +28,29 @@ public class D14 {
             long result = 0L;
             Scanner sc = readFile();
 
+            List<String> _area = new ArrayList<>();
+
             while (sc.hasNext()) {
+                _area.add(sc.nextLine());
+            }
+
+            char[][] area = _area.stream().map(String::toCharArray).toArray(char[][]::new);
+
+            for(int y = 0; y < area[0].length; y++) {
+                int boulders = 0;
+                int square = 0;
+                for(int x = 0; x < area.length; x++) {
+                    if(area[x][y] == 'O') {
+                        result += area.length - square - boulders;
+                        boulders ++;
+                        continue;
+                    }
+                    if(area[x][y] == '#') {
+                        boulders = 0;
+                        square = x+1;
+                        continue;
+                    }
+                }
             }
 
             System.out.println(result);
@@ -48,11 +69,157 @@ public class D14 {
             long result = 0L;
             Scanner sc = readFile();
 
+            List<String> _area = new ArrayList<>();
+
             while (sc.hasNext()) {
+                _area.add(sc.nextLine());
             }
 
+            char[][] area = _area.stream().map(String::toCharArray).toArray(char[][]::new);
+            Map<String,String> cycles = new HashMap<>();
+            String prevArea = toString(area);
+
+            for(int i = 0; i < 1000000000; i++) {
+                if(cycles.containsKey(prevArea)) {
+                    prevArea = cycles.get(prevArea);
+                    area = area;
+                    break;
+                }
+                area = rollNorth(area);
+                area = rollWest(area);
+                area = rollSouth(area);
+                area = rollEast(area);
+                String currentArea = toString(area);
+                cycles.put(prevArea,currentArea);
+                prevArea = currentArea;
+            }
+            int cycleLength = 0;
+            String toCompare = prevArea;
+
+            for(int i = 0; i < 1000000000; i++){
+                cycleLength ++;
+                prevArea = cycles.get(prevArea);
+                if(prevArea.equals(toCompare))
+                    break;
+            }
+
+            for(int i = 0; i < (1000000000 - cycles.size()) % cycleLength; i++) {
+                area = rollNorth(area);
+                area = rollWest(area);
+                area = rollSouth(area);
+                area = rollEast(area);
+            }
+
+            for(int x = 0; x < area[0].length; x++) {
+                int boulders = 0;
+                int square = 0;
+                for(int y = 0; y < area.length; y++) {
+                    if(area[y][x] == 'O') {
+                        result += area.length - y;
+                        boulders ++;
+                        continue;
+                    }
+                    if(area[y][x] == '#') {
+                        boulders = 0;
+                        square = y+1;
+                        continue;
+                    }
+                }
+            }
             System.out.println(result);
             System.out.println("--------------------------------------");
+        }
+
+        public static char[][] rollNorth(char[][] area) {
+            for(int y = 0; y < area[0].length; y++) {
+                int boulders = 0;
+                int square = 0;
+                for(int x = 0; x < area.length; x++) {
+                    if(area[x][y] == 'O') {
+                        area[x][y] = '.';
+                        area[square + boulders][y] = 'O';
+                        boulders ++;
+                        continue;
+                    }
+                    if(area[x][y] == '#') {
+                        boulders = 0;
+                        square = x+1;
+                        continue;
+                    }
+                }
+            }
+            return area;
+        }
+
+        public static char[][] rollSouth(char[][] area) {
+            for(int y = area[0].length -1 ; y >= 0; y--) {
+                int boulders = 0;
+                int square = area[0].length -1;
+                for(int x = area.length-1; x >= 0; x--) {
+                    if(area[x][y] == 'O') {
+                        area[x][y] = '.';
+                        area[square - boulders][y] = 'O';
+                        boulders ++;
+                        continue;
+                    }
+                    if(area[x][y] == '#') {
+                        boulders = 0;
+                        square = x-1;
+                        continue;
+                    }
+                }
+            }
+            return area;
+        }
+
+        public static char[][] rollWest(char[][] area) {
+            for(int x = 0; x < area.length; x++) {
+                int boulders = 0;
+                int square = 0;
+                for(int y = 0; y < area.length; y++) {
+                    if(area[x][y] == 'O') {
+                        area[x][y] = '.';
+                        area[x][square + boulders] = 'O';
+                        boulders ++;
+                        continue;
+                    }
+                    if(area[x][y] == '#') {
+                        boulders = 0;
+                        square = y+1;
+                        continue;
+                    }
+                }
+            }
+            return area;
+        }
+
+        public static char[][] rollEast(char[][] area) {
+            for(int x = 0; x < area.length; x++) {
+                int boulders = 0;
+                int square = area.length-1;
+                for(int y = area.length-1; y >= 0; y--) {
+                    if(area[x][y] == 'O') {
+                        area[x][y] = '.';
+                        area[x][square - boulders] = 'O';
+                        boulders ++;
+                        continue;
+                    }
+                    if(area[x][y] == '#') {
+                        boulders = 0;
+                        square = y-1;
+                        continue;
+                    }
+                }
+            }
+            return area;
+        }
+
+        public static String toString(char[][] area) {
+            StringBuilder sb = new StringBuilder();
+            for(char[] line:area) {
+                sb.append(Arrays.deepToString(area));
+            }
+            return sb.toString();
         }
     }
 }
