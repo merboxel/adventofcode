105e58603cba3c40731495f0f4dad5e7cf564a3a
Max <m.e.r.van.boxel@gmail.com>
2024-12-23 11:59:37 +0100

    2024 D23

--- src/main/java/merboxel/codeofadvent/y2024/D23/D23P1.java
+++ src/main/java/merboxel/codeofadvent/y2024/D23/D23P1.java
@@ -1,9 +1,12 @@
 package merboxel.codeofadvent.y2024.D23;
 
 import merboxel.codeofadvent.AoC;
+import merboxel.codeofadvent.util.ScannerUtil;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class D23P1 extends AoC {
 
@@ -17,11 +20,68 @@ public class D23P1 extends AoC {
 
     public static void main(String[] args) throws IOException {
         System.out.println("--------------- Part 1 ---------------");
-        new D23P1().run();
+        System.out.println(new D23P1().run());
         System.out.println("--------------------------------------");
     }
 
     public String run() {
-        return "";
+
+        List<String> connections = ScannerUtil.toList(sc);
+        Map<String, Node> computers = new HashMap<>();
+
+        for (String connection : connections) {
+            String[] pcs = connection.split("-");
+
+            Node pc1 = computers.getOrDefault(pcs[0], new Node(pcs[0]));
+            Node pc2 = computers.getOrDefault(pcs[1], new Node(pcs[1]));
+
+            pc1.addEdge(pc2);
+            pc2.addEdge(pc1);
+
+            computers.put(pcs[0], pc1);
+            computers.put(pcs[1], pc2);
+        }
+
+        SortedSet<String> cliques = new TreeSet<>();
+
+        for (Node computer : computers.values()) {
+            if (computer.startWithT()) {
+                for (Node computer2 : computer.edges) {
+                    Set<Node> edges = new HashSet<>(computer.edges);
+                    edges.retainAll(computer2.edges);
+                    for (Node computer3 : edges) {
+                        cliques.add(Stream.of(computer.getName(), computer2.getName(), computer3.getName()).sorted(String::compareTo).collect(Collectors.joining(",")));
+                    }
+                }
+            }
+        }
+        return Integer.toString(cliques.size());
+    }
+
+    private static class Node implements Comparable<Node> {
+        String name;
+        Set<Node> edges;
+
+        public Node(String name) {
+            this.name = name;
+            this.edges = new HashSet<>();
+        }
+
+        public void addEdge(Node n) {
+            edges.add(n);
+        }
+
+        public boolean startWithT() {
+            return (name.charAt(0) == 't');
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return this.name.compareTo(that.name);
+        }
     }
 }
--- src/main/java/merboxel/codeofadvent/y2024/D23/D23P2.java
+++ src/main/java/merboxel/codeofadvent/y2024/D23/D23P2.java
@@ -1,9 +1,11 @@
 package merboxel.codeofadvent.y2024.D23;
 
 import merboxel.codeofadvent.AoC;
+import merboxel.codeofadvent.util.ScannerUtil;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class D23P2 extends AoC {
 
@@ -17,11 +19,74 @@ public class D23P2 extends AoC {
 
     public static void main(String[] args) throws IOException {
         System.out.println("--------------- Part 2 ---------------");
-        new D23P2().run();
+        System.out.println(new D23P2().run());
         System.out.println("--------------------------------------");
     }
 
     public String run() {
-        return "";
+
+        List<String> connections = ScannerUtil.toList(sc);
+        Map<String, Node> computers = new HashMap<>();
+
+        for (String connection : connections) {
+            String[] pcs = connection.split("-");
+
+            Node pc1 = computers.getOrDefault(pcs[0], new Node(pcs[0]));
+            Node pc2 = computers.getOrDefault(pcs[1], new Node(pcs[1]));
+
+            pc1.addEdge(pc2);
+            pc2.addEdge(pc1);
+
+            computers.put(pcs[0], pc1);
+            computers.put(pcs[1], pc2);
+        }
+
+        Set<Node> dfs = dfs(new HashSet<>(computers.values()), new HashSet<>());
+
+        return dfs.stream().sorted(Node::compareTo).map(Node::getName).collect(Collectors.joining(","));
+    }
+
+    private Set<Node> dfs(Set<Node> edges, Set<Node> visited) {
+
+        Set<Node> result = visited;
+        Set<Node> seen = new HashSet<>();
+
+        for (Node computer : edges) {
+            Set<Node> copyEdges = new HashSet<>(edges);
+            copyEdges.removeAll(seen);
+            Set<Node> copyVisited = new HashSet<>(visited);
+            copyVisited.add(computer);
+            copyEdges.retainAll(computer.edges);
+            Set<Node> dfs = dfs(copyEdges, copyVisited);
+            if (result.size() < dfs.size()) {
+                result = dfs;
+            }
+            seen.add(computer);
+        }
+
+        return result;
+    }
+
+    private static class Node implements Comparable<Node> {
+        final String name;
+        final Set<Node> edges;
+
+        public Node(String name) {
+            this.name = name;
+            this.edges = new HashSet<>();
+        }
+
+        public void addEdge(Node n) {
+            edges.add(n);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return this.name.compareTo(that.name);
+        }
     }
 }
